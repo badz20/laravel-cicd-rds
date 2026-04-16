06afaff04900b288f8ba58eca91310acc236d189
badrul <mhdbadrul.masrol@myeg.com.my>
2026-04-17 05:05:09 +0800

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -9,10 +9,10 @@ class LoginController extends Controller
 {
     use AuthenticatesUsers;
 
-    protected $redirectTo = '/admin';
+    protected $redirectTo = "/admin";
 
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        $this->middleware("guest")->except("logout");
     }
 }
